0bcf7ab195e6d5a582158753e1ad3ed3cb89bc76
MS1317 <122727185+MS1317@users.noreply.github.com>
2023-01-15 19:29:33 +0530

    Update kassflora_jquery.js

--- kassflora_jquery.js
+++ kassflora_jquery.js
@@ -1,30 +1,30 @@
 
-function indexMedium(size){
-let button = document.getElementById('openbtn');
-let nav_icon=document.getElementById('nav-icons');
-if (button !== null) {
-	if (size.matches) 
-	{
-		button.style.display ="block";
-		nav_icon.style.display="none";
-		$('#openbtn').addClass('container-fluid');
-		$('#logo').removeClass('center');
+// function indexMedium(size){
+// let button = document.getElementById('openbtn');
+// let nav_icon=document.getElementById('nav-icons');
+// if (button !== null) {
+// 	if (size.matches) 
+// 	{
+// 		button.style.display ="block";
+// 		nav_icon.style.display="none";
+// 		$('#openbtn').addClass('container-fluid');
+// 		$('#logo').removeClass('center');
 
 
-	}
-	else
-	{
-		button.style.display="none";
-		nav_icon.style.display="block";
-		$('#openbtn').removeClass('container-fluid');
-		$('#logo').addClass('center');
-	}
-}
-}
+// 	}
+// 	else
+// 	{
+// 		button.style.display="none";
+// 		nav_icon.style.display="block";
+// 		$('#openbtn').removeClass('container-fluid');
+// 		$('#logo').addClass('center');
+// 	}
+// }
+// }
 
-var size = window.matchMedia("(max-width:658px)");
-indexMedium(size);
-size.addListener(indexMedium);
+// var size = window.matchMedia("(max-width:658px)");
+// indexMedium(size);
+// size.addListener(indexMedium);
 
 function openNav()
 {
@@ -38,32 +38,32 @@ function closeNav()
   document.getElementById("mySidebar").style.width = "0";
 }	
 
-function indexSmall(s_size)
-{
-	if (s_size.matches) 
-	{
-		$('#logo').addClass('center');
-	}
-	else
-		$('#logo').removeClass('center');
-}
+// function indexSmall(s_size)
+// {
+// 	if (s_size.matches) 
+// 	{
+// 		$('#KFlogo').addClass('center');
+// 	}
+// 	else
+// 		$('#KFlogo').removeClass('center');
+// }
 
 
-var s_size=window.matchMedia("(max-width:360px)");
-indexSmall(s_size);
-s_size.addListener(indexSmall)
+// var s_size=window.matchMedia("(max-width:360px)");
+// indexSmall(s_size);
+// s_size.addListener(indexSmall)
 
-function indexSmall2(s_size2)
-{
-	if (s_size2.matches) 
-	{
-		$('#sign').addClass('center');
-	}
-	else
-		$('#sign').removeClass('center');
-}
+// function indexSmall2(s_size2)
+// {
+// 	if (s_size2.matches) 
+// 	{
+// 		$('#sign').addClass('center');
+// 	}
+// 	else
+// 		$('#sign').removeClass('center');
+// }
 
 
-var s_size2=window.matchMedia("(max-width:360px)");
-indexSmall2(s_size2);
-s_size2.addListener(indexSmall2)
+// var s_size2=window.matchMedia("(max-width:360px)");
+// indexSmall2(s_size2);
+// s_size2.addListener(indexSmall2)
